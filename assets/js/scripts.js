e067533d5a7efbee2df64c7fd612767b351bfe53
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-03-07 18:55:34 +0100

    First attempt

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleBurgerMenu() {
+    var x = document.getElementsByTagName("nav")[0];
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
